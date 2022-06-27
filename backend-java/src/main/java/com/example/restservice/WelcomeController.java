d40abe301529d11c77a676a95013465859835bcd
hiakki <hy.akshay@gmail.com>
2022-06-27 16:59:56 +0530

    Updated java controller for CORS issue

--- backend-java/src/main/java/com/example/restservice/WelcomeController.java
+++ backend-java/src/main/java/com/example/restservice/WelcomeController.java
@@ -2,6 +2,7 @@ package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-	private static final String template = "My name is %s!";
+	private static final String template = "My Name is, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	@CrossOrigin(origins = {"http://localhost:3000"})
 	@GetMapping("/welcome")
 	public Welcome welcome(@RequestParam(value = "name", defaultValue = "hiAkki") String name) {
 		return new Welcome(counter.incrementAndGet(), String.format(template, name));
